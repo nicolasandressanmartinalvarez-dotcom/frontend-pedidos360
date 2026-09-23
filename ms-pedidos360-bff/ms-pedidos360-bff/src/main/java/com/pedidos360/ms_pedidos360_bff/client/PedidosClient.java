62ae09dbfa02c6977886a5ba50c43e0b9289c5e1
Jmauriz559 <jmauriz559@gmail.com>
2026-09-23 12:15:13 -0300

    ,

--- ms-pedidos360-bff/ms-pedidos360-bff/src/main/java/com/pedidos360/ms_pedidos360_bff/client/PedidosClient.java
+++ ms-pedidos360-bff/ms-pedidos360-bff/src/main/java/com/pedidos360/ms_pedidos360_bff/client/PedidosClient.java
@@ -2,6 +2,8 @@ package com.pedidos360.ms_pedidos360_bff.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Map;
 
@@ -10,4 +12,8 @@ public interface PedidosClient {
 
     @GetMapping("/api/orders")
     List<Map<String, Object>> obtenerPedidos();
+
+    // --- NUEVO: Método para enviar la creación del pedido al microservicio ---
+    @PostMapping("/api/orders")
+    Map<String, Object> crearPedido(@RequestBody Map<String, Object> pedido);
 }
